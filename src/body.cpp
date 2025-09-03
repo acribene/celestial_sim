b460b477a28058720f14c2cb7e2de67dbdb7383e
Connor <92689793+acribene@users.noreply.github.com>
2025-09-03 12:41:16 -0700

    Update body.cpp
    Fixed some issues with op =

--- src/body.cpp
+++ src/body.cpp
@@ -11,20 +11,20 @@ Body::Body(Body &&rhs) noexcept : m_position(std::move(rhs.m_position)), m_veloc
 
 inline Body &Body::operator=(const Body &rhs)
 {
-    m_position = Vec2();
-    m_velocity = Vec2();
-    m_acceleration = Vec2();
-    m_mass = 0;
+    m_position = rhs.m_position;
+    m_velocity = rhs.m_velocity;
+    m_acceleration = rhs.m_acceleration;
+    m_mass = rhs.mass;
 
     return *this;
 }
 
 inline Body &Body::operator=(Body &&rhs) noexcept
 {
-    m_position = Vec2();
-    m_velocity = Vec2();
-    m_acceleration = Vec2();
-    m_mass = 0;
+    m_position = rhs.m_position;
+    m_velocity = rhs.m_velocity;
+    m_acceleration = rhs.m_acceleration;
+    m_mass = rhs.mass
 
     return *this;
 }
