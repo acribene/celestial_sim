6a83a7e8cf18d1a27103cb12fa11ef9e080373c7
Connor <92689793+acribene@users.noreply.github.com>
2025-09-03 12:48:05 -0700

    Update constants.h
    Altered and added some new things to constants file

--- src/utils/constants.h
+++ src/utils/constants.h
@@ -1,12 +1,21 @@
+#ifndef CONSTANTS_H
+define CONSTANTS_H
+
 #include <cmath>
 
 // Header file full of constants for whatever reasons we may need.
 const constexpr double DAYS_PER_YEAR = 365.25;
+const double HOURS_PER_DAY = 24.0;
+const double MINUTES_PER_HOUR = 60.0;
+const double SECONDS_PER_MINUTE = 60.0;
 
-const constexpr double GC = (4.0 * PI * PI); // Gravitational constant -- one for now
+const constexpr double GC = (4.0 * PI * PI); // Gravitational constant
 const double DT = 1.0 / DAYS_PER_YEAR;
-//const double T_MIN = 0.
 
 // RAYLIB CONSTANTS
 const int WINDOW_WIDTH = 1200;
 const int WINDOW_HEIGHT = 800;
+const constexpr double TIME_STEP = 1.0 / 365.25; // 1 day in years
+const double SCALE = 100.0; // pixels per AU
+
+#endif // CONSTANTS_H
