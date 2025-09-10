8a1de75e066e99697bbe5efaaa1405b22098b9d7
Connor Harper <connor.harper@oit.edu>
2025-09-09 18:43:23 -0700

    fleshed out our time manager some more

--- src/TimeManager.cpp
+++ src/TimeManager.cpp
@@ -4,3 +4,46 @@
 TimeManager::TimeManager(double fixedDeltaTime) : m_currentTime(clocktype_t::now()), m_accumulator(0), m_timeScale(1),
     m_fixedDeltaTime(fixedDeltaTime), m_maxFrameTime(0.1)
 {}
+
+// Updates the time elapsed and sets up framtime for physics calculations
+void TimeManager::update()
+{
+    auto new_time = clocktype_t::now();
+    seconds_t frameTime = new_time - m_currentTime;
+    m_currentTime = new_time;
+
+    // Clamping the frame time so lags don't spiral
+    frameTime = std::min(frameTime, m_maxFrameTime);
+    m_accumulator += frameTime;
+}
+
+double TimeManager::getTimeScale() const
+{
+    return m_timeScale;
+}
+
+void TimeManager::setTimeScale(double scale)
+{
+    m_timeScale = std::max(0.0, scale);
+}
+
+void TimeManager::increaseTimeScale(float factor)
+{
+    setTimeScale(m_timeScale * factor);
+}
+
+void TimeManager::decreaseTimeScale(float factor)
+{
+    setTimeScale(m_timeScale / factor);
+}
+
+bool TimeManager::shouldUpdatePhysics() const
+{
+    return m_accumulator >= m_fixedDeltaTime;
+}
+
+// Subtracts our delta time from our accumulator
+void TimeManager::consumePhysicsTime()
+{
+    m_accumulator -= m_fixedDeltaTime;
+}
--- src/TimeManager.h
+++ src/TimeManager.h
@@ -8,11 +8,11 @@ using timepoint_t = std::chrono::time_point<clocktype_t>;
 
 class TimeManager {
 private:
-  timepoint_t m_currentTime;
-  double m_accumulator;
-  double m_timeScale;
-  double m_fixedDeltaTime;
-  double m_maxFrameTime;
+  timepoint_t m_currentTime; // The current time
+  seconds_t m_accumulator; // Accumulates actual time passed between frames
+  double m_timeScale; // The rate at which simulation is running, ie.. 1.0 == real time
+  seconds_t m_fixedDeltaTime; // The rate at which we want our physics updating
+  seconds_t m_maxFrameTime; // Used to clamp our measured frame time in the event of lag
 
 public:
   TimeManager(double fixedDeltaTime = 1.0/60);
@@ -20,7 +20,7 @@ public:
   void update();
   double getFixedDeltaTime() const;
   double getTimeScale() const;
-  void setTimeScale();
+  void setTimeScale(double scale);
   void increaseTimeScale(float factor);
   void decreaseTimeScale(float factor);
   bool shouldUpdatePhysics() const;
