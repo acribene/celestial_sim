ec744b944a0dd8577c2c857c3a8af164ba4ddf0d
Connor Harper <connorharper993@gmail.com>
2025-09-16 16:38:06 -0700

    added first iteration of application

--- src/headers/Application.h
+++ src/headers/Application.h
@@ -1,9 +1,36 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
 
+#include "simulation.h"
+#include "TimeManager.h"
+#include "CameraController.h"
+#include "InputHandler.h"
+#include "../utils/constants.h"
+
 class Application
 {
+    private:
+    bool isRunning;
+    CameraController cameraController;
+    TimeManager timeManager;
+    Simulation simulation;
 
+    void initialize();
+    void update();
+    void render();
+    void shutdown();
+    void processInput();
+
+    public:
+    // Takes the window width and height
+    Application(int width, int height);
+    ~Application() = default;
+
+    // Wont support copying of Applications
+    Application(const Application& app) = delete;
+    Application(Application&& app) = delete;
+
+    void run();
 };
 
 #endif // APPLICATION_H
---
+++ src/source/Application.cpp
@@ -0,0 +1,75 @@
+#include "../headers/Application.h"
+#include "raylib.h"
+#include <iostream>
+
+Application::Application(int width, int height) : isRunning(false), cameraController(width, height), timeManager(TimeManager()), simulation(Simulation()) {
+    initialize();
+}
+
+void Application::initialize() {
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "2D Physics Simulator");
+    SetTargetFPS(60);
+    
+    // For testing
+    Body sun(1.0, 0.05, Vec2(0, 0), Vec2(0, 0), WHITE);
+    Body earth(0.000003, 0.01, Vec2(1.0, 0), Vec2(0, 2.0 * M_PI), WHITE);
+    Body mars(0.00000032, 0.005, Vec2(1.52, 0), Vec2(0, 5.08), WHITE);
+    
+    simulation.addBody(sun);
+    simulation.addBody(earth);
+    simulation.addBody(mars);
+    
+    isRunning = true;
+}
+
+void Application::update()
+{
+    int count = 0;
+    timeManager.update();
+    while(timeManager.shouldUpdatePhysics())
+    {
+        simulation.update(timeManager.getFixedDeltaTime());
+        timeManager.consumePhysicsTime();
+        std::cout << "Update Count: " << count++ << std::endl;
+    }
+}
+
+void Application::render()
+{
+    BeginDrawing();
+    ClearBackground(BLACK);
+    
+    BeginMode2D(cameraController.getCamera());
+    simulation.render();
+    EndMode2D();
+
+    // Display information
+    DrawText("2D Physics Simulator", 10, 10, 20, WHITE);
+    DrawText(TextFormat("Time Scale: %.2f", timeManager.getTimeScale()), 10, 40, 20, WHITE);
+    DrawText(TextFormat("FPS: %.0f", GetFPS()), 10, 70, 20, WHITE);
+    DrawText("Controls: Arrow keys to pan, A/Z to zoom, +/- to change time scale", 10, 100, 20, WHITE);
+
+    EndDrawing();
+}
+
+void Application::shutdown()
+{
+    CloseWindow();
+}
+
+void Application::processInput()
+{
+    InputHandler::handleTimeScaleInput(timeManager);
+    InputHandler::handleCameraInput(cameraController);
+}
+
+void Application::run()
+{
+    while (isRunning && !WindowShouldClose())
+    {
+        processInput();
+        update();
+        render();
+    }
+    shutdown();
+}
