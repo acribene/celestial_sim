496537bb005a86fca5609d2f42eaacb99293a77a
Connor Harper <connor.harper@oit.edu>
2026-01-25 17:30:43 -0800

    Added raygui and started stubbing out sidbar gui class

---
+++ src/headers/Sidebar.h
@@ -0,0 +1,41 @@
+
+#ifndef SIDEBAR_H
+#define SIDEBAR_H
+#include "raylib.h"
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+#include "Body.h"
+
+// Enum for sidebar tabs
+enum SidebarTab {
+    INSPECTOR,
+    SETTINGS
+};
+
+class Sidebar {
+private:
+    // Layout
+    Rectangle bounds_;
+    float expandedWidth_ = 300.0f;
+    float currentWidth_ = 0.0f;
+    bool isOpen_ = false;
+    
+    // State
+    Body* selectedBody_ = nullptr; // TODO: May change because of possible invalid pointers when bodies are removed
+    SidebarTab currentTab_ = SidebarTab::INSPECTOR;
+
+public:
+    Sidebar();
+    
+    // Core loop
+    void update(float dt);
+    void render();
+
+    // Selection logic
+    void selectBody(Body* body);
+    void deselect();
+    bool isMouseOver(); // Helper to prevent clicking through the UI
+    bool hasSelection() const { return selectedBody_ != nullptr; }
+};
+
+#endif // SIDEBAR_H
